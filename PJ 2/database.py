e1eab217c5c08950704927ae3a7d4e04bbf8548b
rodrigocrespo <rod_crespo@outlook.com>
2017-03-30 14:55:35 -0300

    data base

---
+++ PJ 2/database.py
@@ -0,0 +1,34 @@
+import pickle
+database=[{
+	"nome" : "Squirtle",
+	"poder" : 20,
+	"vida" : 200,
+	"defesa" : 12
+},
+{
+	"nome" : "caterpie",
+	"poder" : 25,
+	"vida" : 100,
+	"defesa" : 10
+},
+{
+	"nome" : "Bulbasaur",
+	"poder" : 15,
+	"vida" : 300,
+	"defesa" : 11
+},
+{
+	'nome' : 'Pidgey',
+	'poder' : 27,
+	'vida' : 200,
+	'defesa' : 13 
+},
+{'player': {
+'nome' : 'pikachu',
+'poder' : 35,
+'vida' : 150,
+'defesa' : 12 }
+}
+]
+with open ('database.pickle','wb') as arquivo:
+	pickle.dump(database,arquivo)
