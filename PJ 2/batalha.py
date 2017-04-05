5605cc5361a01fd9f64d75be6f7634fd53cac723
Parizotto <vitorpms@al.insper.edu.br>
2017-04-05 18:26:42 -0300

    Função batalhafuncionando com pequenos ajustes nas equações(da própria batalha

--- PJ 2/batalha.py
+++ PJ 2/batalha.py
@@ -2,13 +2,13 @@ PERDEU = 0
 VENCEU = 1
 
 def batalha(player, inimigo):
-	while player[vida]>0 and inimigo[vida]>0:
-		Vidainimigo = inimigo[vida] - ( player[poder] - inimigo[defesa])
-		if Vidainimigo <=0 :
-			return VENCEU # conferir se deve ser retotnado 1 ou venceu
-		Vidaplayer = player[vida] - (inimido[poder] - player[defesa])
-		if Vidaplayer <=0:
-			return PERDEU	
+	while player["vida"]>0 and inimigo["vida"]>0:
+		inimigo["vida"] = inimigo["vida"] + ( inimigo["defesa"] -player["poder"])
+		player["vida"] = player["vida"] + (player["defesa"]-inimigo["poder"] )
+	if  inimigo["vida"]<=0 :
+		return VENCEU
+	if player["vida"] <=0:
+		return PERDEU	
 	# if (inimigo['poder']) >= (player['poder']):
 	# 	batalha()
 	# else:
