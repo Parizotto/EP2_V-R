74215f101ca36b1d914b96e0586838555301db85
rodrigocrespo <rod_crespo@outlook.com>
2017-04-06 16:05:51 -0300

    programa quase com delay de print

--- PJ 2/escolha.py
+++ PJ 2/escolha.py
@@ -1,15 +1,19 @@
 PASSEAR = 0
 DORMIR = 1
-
+import time, sys
 def escolha():
 	while True:
+		hue = "vamos la.....\o/\n"
 		resposta = input("Você deseja passear ou dormir?")
 		if resposta == "passear":
-			print("vamos la..... \o/")
+			for character in hue:
+				sys.stdout.write(character)
+				sys.stdout.flush()
+				time.sleep(0.2)
 			return PASSEAR
 		if resposta == "dormir":
 			print("okay")
 			return DORMIR
 		else:
 			print("nao existe")
-	
+escolha()
