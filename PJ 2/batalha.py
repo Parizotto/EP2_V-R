3e475744d820dd8779f438f92c68d581ee6bf229
Parizotto <vitorpms@al.insper.edu.br>
2017-04-06 16:12:54 -0300

    programa feito batalha

--- PJ 2/batalha.py
+++ PJ 2/batalha.py
@@ -2,21 +2,46 @@ PERDEU = 0
 VENCEU = 1
 
 def batalha(player, inimigo):
-	while player["vida"]>0 and inimigo["vida"]>0:
-		inimigo["vida"] = inimigo["vida"] + ( inimigo["defesa"] -player["poder"])
-		player["vida"] = player["vida"] + (player["defesa"]-inimigo["poder"] )
-	if  inimigo["vida"]<=0 :
-		return VENCEU
-	if player["vida"] <=0:
-		return PERDEU	
-	# if (inimigo['poder']) >= (player['poder']):
-	# 	FAZER A BATALHA
-	# else:
+        resposta= int(input("você quer fugir(1) ou batalhar(2)"))
+        while True:
+                if resposta == 1:
+                        if (inimigo["poder"]) <= (player["poder"]):
+                                print("hue")
+                                break
+                        else:
+                                        print("Você não pode fugir")
+                                        while player["vida"]>0 and inimigo["vida"]>0:
+                                                inimigo["vida"] = inimigo["vida"] + ( inimigo["defesa"] -player["poder"])
+                                                player["vida"] = player["vida"] + (player["defesa"]-inimigo["poder"] )
+                                        if  inimigo["vida"]<=0 :
+                                                return VENCEU
+                                        if player["vida"] <=0:
+                                                return PERDEU
+                if resposta == 2:
+                                while player["vida"]>0 and inimigo["vida"]>0:
+                                        inimigo["vida"] = inimigo["vida"] + ( inimigo["defesa"] -player["poder"])
+                                        player["vida"] = player["vida"] + (player["defesa"]-inimigo["poder"] )
+                                if  inimigo["vida"]<=0 :
+                                        return VENCEU
+                                if player["vida"] <=0:
+                                        return PERDEU
+
+
 	# 	resposta= int(input('você quer fugir(1) ou batalhar(2)'))
 	# 	if resposta == 1:
-	# 		continue
+	#              if (inimigo['poder']) >= (player['poder']):
+	#                       print("Você não pode fugir")
+	# 	       continue
 	# 	if resposta == 2:
-	# 		batalha()
+	#               while player["vida"]>0 and inimigo["vida"]>0:
+	#	                inimigo["vida"] = inimigo["vida"] + ( inimigo["defesa"] -player["poder"])
+	#	                player["vida"] = player["vida"] + (player["defesa"]-inimigo["poder"] )
+	#               if  inimigo["vida"]<=0 :
+	#	                return VENCEU
+	#               if player["vida"] <=0:
+	#	                return PERDEU
+	# 		
+	# 	
 	# 	else:
 	# 		print('nao existe essa opção')
 	# 		continue
