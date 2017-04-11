d99a1e5ed3b5907d0bd99be1b563360c914b43dd
rodrigocrespo <rod_crespo@outlook.com>
2017-04-11 11:54:39 -0300

    adicionei a funcao aleatoria na batalha de fugir

--- PJ 2/batalha.py
+++ PJ 2/batalha.py
@@ -1,24 +1,46 @@
 PERDEU = 0
 VENCEU = 1
-
+import random
 def batalha(player, inimigo):
-	resposta = int(input("você quer fugir(1) ou batalhar(2)"))	
+	fugir_lento = [1,2,3,4,5,6,7,8,9,10]
+	fugir_rapido = [1,2,3,4,5]
+	run= ("Fugiu")
+	resposta = int(input("\nvocê deseja:\n1 - fugir\n   ou\n2 - batalhar\n"))	
 	while True:
-		if resposta == 1:
-			if (player["poder"]) >= (inimigo["poder"]):
-				print ("fugiu")
-				break
-			else:
-				print("Você não pode fugir")
-				while player["vida"]>0 and inimigo["vida"]>0:
-					inimigo["vida"] = inimigo["vida"] + ( inimigo["defesa"] -player["poder"])
-					player["vida"] = player["vida"] + (player["defesa"]-inimigo["poder"] )
-					if inimigo["vida"]<=0:
-						return VENCEU
-						break
-					if player["vida"] <=0:
-						return PERDEU
-						break
+		conseguiu = ("\nsucesso na fuga\n")
+		if resposta == 1: #se quiser fugir ele vai rodar as opçoes
+			if (player["poder"]) >= (inimigo["poder"]): #chance alta de fugir
+				run_fast = fugir_rapido[random.randrange(len(fugir_rapido))]
+				if run_fast == 1 or run_fast ==3 or run_fast == 5:
+					print("Fugiu")
+					break
+				else:
+					print("\nVocê não pode fugir\n")
+					while player["vida"]>0 and inimigo["vida"]>0:
+						inimigo["vida"] = inimigo["vida"] + ( inimigo["defesa"] -player["poder"])
+						player["vida"] = player["vida"] + (player["defesa"]-inimigo["poder"] )
+						if inimigo["vida"]<=0:
+							return VENCEU
+							break
+						if player["vida"] <=0:
+							return PERDEU
+							break
+			if (player["poder"]) <= (inimigo["poder"]):
+				run_slow = fugir_lento[random.randrange(len(fugir_lento))]
+				if run_slow == 3 or run_slow ==8 or run_slow == 5:
+					print("Fugiu")
+					break
+				else:
+					print("\nVocê não pode fugir\n")
+					while player["vida"]>0 and inimigo["vida"]>0:
+						inimigo["vida"] = inimigo["vida"] + ( inimigo["defesa"] -player["poder"])
+						player["vida"] = player["vida"] + (player["defesa"]-inimigo["poder"] )
+						if inimigo["vida"]<=0:
+							return VENCEU
+							break
+						if player["vida"] <=0:
+							return PERDEU
+							break
 		if resposta == 2:
 			while player["vida"]>0 and inimigo["vida"]>0:
 				inimigo["vida"] = inimigo["vida"] + ( inimigo["defesa"] -player["poder"])
